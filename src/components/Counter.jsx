28d95daa7b78c2ebd800cd5dbad1195dbeebf1bb
Hesennov <hasanhasanov345@gmail.com>
2023-06-03 00:29:03 +0400

    createElement()

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,12 +1,13 @@
 import useCounter from "../hooks/use-counter";
-
+import "../App.css";
 const Counter = () => {
-  const { count, increment, decrement } = useCounter();
+  const { count, increment, decrement, NegativeContent } = useCounter();
   return (
     <div>
       <button onClick={increment}>artir</button>
       {count}
       <button onClick={decrement}>azalt</button>
+      <NegativeContent className={"red"}>cok indin aga</NegativeContent>
     </div>
   );
 };
--- src/hooks/use-counter.jsx
+++ src/hooks/use-counter.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { Children, createElement, useState } from "react";
 
 export default function useCounter() {
   const [count, setCount] = useState(2);
@@ -7,10 +7,20 @@ export default function useCounter() {
 
   const decrement = () => setCount((count) => count - 1);
 
+  const NegativeContent = ({ children, ...props }) => {
+    if (count < 0) {
+      return createElement("div", props, children);
+      // <div>{children}</div>;
+    }
+    {
+      return null;
+    }
+  };
+
   return {
     count,
     increment,
     decrement,
+    NegativeContent,
   };
 }
-  
